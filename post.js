c74d1e20a578cb782773aaef401be1eaadbc9a00
Kevin Kamto <kamtokevin@gmail.com>
2020-07-14 09:58:11 -0400

    refactor(post): rewrote in oop

--- post.js
+++ post.js
@@ -1,12 +1,16 @@
 const { default: Axios } = require('axios');
-let url = "https://industrial.api.ubidots.com/api/v1.6/devices/dock-sensor/";
+let apiUrl = "https://industrial.api.ubidots.com/api/v1.6/devices/dock-sensor/";
 
-setInterval(() => {
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-      }
-      let data = {
+function Request(url)
+{
+    const __proto__ = {}
+    __proto__.destination = url;
+    __proto__.options = {
+        headers: '',
+        
+    };
+    __proto__.data = {
         "water-level": getRandomInt(12),
         "first_name": "Kevin",
         "last_name": "Kamto",
@@ -18,7 +22,24 @@ setInterval(() => {
         "diff": Math.random()
     }
 
+    // Functions
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    __proto__.sendHttpPost = function () {
+        // Send Post request
+        Axios.post(__proto__.destination, __proto__.data, { headers: {  'X-AUTH-TOKEN': 'BBFF-Ee83o8g7QnbULoPZ4QliMGiTYPiLqL',
+        'content-type': 'application/json'}}).then((response)=>{console.log(response.data)}).catch(error => {console.log(`\n\n\n Caught Promise Rejection: ${error}`)})
+    }
 
-    Axios.post(url, data, { headers: {  'X-AUTH-TOKEN': 'BBFF-Ee83o8g7QnbULoPZ4QliMGiTYPiLqL',
-    'content-type': 'application/json'}}).then((response)=>{console.log(response.data)}).catch(error => {console.log(`\n\n\n Caught Promise Rejection: ${error}`)})
-}, 500);
+    __proto__.simulateSensor = function() {
+        setInterval(() => {
+            __proto__.sendHttpPost();
+        }, 500)
+    }
+
+    return __proto__;
+}
+
+var Ticker = Request(apiUrl);
+Ticker.simulateSensor();
